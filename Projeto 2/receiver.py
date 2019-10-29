86202e4f946343e05e51bfbef947f9701518f575
Joao Moraes <23438882+joaoadm94@users.noreply.github.com>
2019-10-29 17:53:27 -0300

    stop and wait funcionando

--- Projeto 2/receiver.py
+++ Projeto 2/receiver.py
@@ -1,11 +1,14 @@
 import socket
 import sys
+import os
+import time
 
 host = '127.0.0.1'
 port = 5000
 endereco_local = (host, port)
 mensagem = ''
 prox_msg = '0'
+receber = True
 
 try:
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -15,13 +18,31 @@ except socket.error:
     sys.exit()
 
 sock.bind(endereco_local)
+if os.path.exists("recebido.jpg"):
+    os.remove("recebido.jpg")
 
-while True:
+while (receber):
     msg, address = sock.recvfrom(4096)
-    print(msg)
-    if msg[0] == '1':
-        prox_msg = '0'
+    if msg == 'INICIO':
+        resp = 'ACK INICIO'
+        sock.sendto(resp.encode(), address)
+        arquivo = open("recebido.jpg", "w+")
+        continue
     else:
-        prox_msg = '1'
-    resp = 'ACK ' + prox_msg
-    sock.sendto(resp.encode(), address)
+        if msg == 'FIM':
+            print('Transferencia finalizada')
+            receber = False
+            arquivo.close()
+        else:
+            print(msg[0])
+            if msg[0] == prox_msg:
+                payload = bytearray(msg)
+                arquivo.write(payload[4:])
+            if msg[0] == '1':
+                prox_msg = '0'
+            else:
+                prox_msg = '1'
+            resp = 'ACK ' + prox_msg
+            sock.sendto(resp.encode(), address)
+
+sock.close()
--- Projeto 2/sender.py
+++ Projeto 2/sender.py
@@ -1,14 +1,19 @@
 import socket
 import sys
 import time
+import random
 
 host = "127.0.0.1"
 port_receiver = 5000
 dest = (host, port_receiver)
 mensagem = 'Redes de Computadores'
+inicio = 'INICIO'
+fim = 'FIM'
+ack_inicio = True
 
-header = '0'
+bit = '0'
 index = 0
+buffer_size = 2048
 
 try: 
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -16,25 +21,52 @@ except socket.error:
     print('Falha ao criar socket')
     sys.exit()
 
-while True:
-    msg = header + ', ' + mensagem
-    sent = sock.sendto(msg.encode(), dest)
-    tempo = time.time()
+try:
+    arquivo_imagem = open("unb.jpg", "r")
+except:
+    print("Erro na abertura do arquivo. Encerrando...")
+
+conteudo = arquivo_imagem.read(buffer_size)
+while (ack_inicio):
+    sock.sendto(inicio.encode(), dest)
     sock.settimeout(5)
-    resp = ''
     try: 
-        resp = sock.recvfrom(4096)
+        resp, address = sock.recvfrom(1024)
     except socket.timeout:
         print('Nenhum dado recebido. Reenviando...')
         time.sleep(1)
         continue
+    if resp == 'ACK INICIO':
+        break
+
+while (conteudo):
+    header = bit + '$*$'
+    sent = sock.sendto(header.encode() + conteudo, dest)
+    tempo = time.time()
+    sock.settimeout(5)
+    resp = ''
+
+    try: 
+        resp, address = sock.recvfrom(1024)
+    except socket.timeout:
+        print('Nenhum dado recebido. Reenviando...')
+        #time.sleep(1)
+        continue
+
     print(resp)
     if resp <> '':
-        if header == '1':
-            header = '0'
+        if resp[4] == bit:
+            continue
+        if bit == '1':
+            bit = '0'
         else:
-            header = '1'
-    time.sleep(1)
+            bit = '1'
+    conteudo = arquivo_imagem.read(buffer_size)
+    #time.sleep(1)
+
+sent = sock.sendto(fim.encode(), dest)
+sock.close()
+
         
 
 
