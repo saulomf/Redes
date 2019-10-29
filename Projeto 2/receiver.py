27bcd22edbd4db0d7981fc83ad59c606770451ab
Joao Moraes <23438882+joaoadm94@users.noreply.github.com>
2019-10-29 18:51:18 -0300

    adicionando mecanismo de perda de pacotes + comentarios

--- Projeto 2/receiver.py
+++ Projeto 2/receiver.py
@@ -2,6 +2,7 @@ import socket
 import sys
 import os
 import time
+import random
 
 host = '127.0.0.1'
 port = 5000
@@ -10,6 +11,7 @@ mensagem = ''
 prox_msg = '0'
 receber = True
 
+# Criacao do socket
 try:
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
@@ -17,32 +19,48 @@ except socket.error:
     print('Falha ao criar socket')
     sys.exit()
 
+# Binding do socket para que fique aberto e aguardando contato
 sock.bind(endereco_local)
+
+# Deleta o arquivo a ser recebido se ele ja existir
 if os.path.exists("recebido.jpg"):
     os.remove("recebido.jpg")
 
+# Loop de recebimento de dados
 while (receber):
     msg, address = sock.recvfrom(4096)
+
+    # Condicao para abertura do arquivo: recebimento da mensagem
+    # de INICIO da transferencia
     if msg == 'INICIO':
         resp = 'ACK INICIO'
         sock.sendto(resp.encode(), address)
         arquivo = open("recebido.jpg", "w+")
         continue
     else:
+        # Condicao para fechamento do arquivo: recebimento
+        # da mensagem de FIM da transferencia
         if msg == 'FIM':
             print('Transferencia finalizada')
             receber = False
             arquivo.close()
         else:
+            # Trecho que lida com o recebimento do arquivo em si
             print(msg[0])
+
+            # Se o pacote eh o que esta sendo esperado: adiciona
+            # os dados no arquivo (descarta o header) e atualiza 
+            # o bit do proximo pacote esperado. Do contrario, reenvia
+            # o ACK. 
             if msg[0] == prox_msg:
                 payload = bytearray(msg)
                 arquivo.write(payload[4:])
-            if msg[0] == '1':
-                prox_msg = '0'
-            else:
-                prox_msg = '1'
+                if msg[0] == '1':
+                    prox_msg = '0'
+                else:
+                    prox_msg = '1'
             resp = 'ACK ' + prox_msg
-            sock.sendto(resp.encode(), address)
+            if random.randint(0, 20) <> 13:
+                sock.sendto(resp.encode(), address)
 
 sock.close()
--- Projeto 2/sender.py
+++ Projeto 2/sender.py
@@ -3,33 +3,36 @@ import sys
 import time
 import random
 
+
 host = "127.0.0.1"
 port_receiver = 5000
-dest = (host, port_receiver)
-mensagem = 'Redes de Computadores'
+dest = (host, port_receiver) # destinatario dos pacotes
+
 inicio = 'INICIO'
 fim = 'FIM'
 ack_inicio = True
-
 bit = '0'
 index = 0
 buffer_size = 2048
 
+# Criacao do socket
 try: 
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 except socket.error:
     print('Falha ao criar socket')
     sys.exit()
 
+# Abrindo o arquivo de imagem
 try:
     arquivo_imagem = open("unb.jpg", "r")
 except:
     print("Erro na abertura do arquivo. Encerrando...")
 
-conteudo = arquivo_imagem.read(buffer_size)
+# Iniciando a conexao, a imagem nao comeca a ser enviada ate a primeira
+# mensagem ser respondida
 while (ack_inicio):
     sock.sendto(inicio.encode(), dest)
-    sock.settimeout(5)
+    sock.settimeout(0.1)
     try: 
         resp, address = sock.recvfrom(1024)
     except socket.timeout:
@@ -39,13 +42,23 @@ while (ack_inicio):
     if resp == 'ACK INICIO':
         break
 
+# Inicio do envio da imagem
+conteudo = arquivo_imagem.read(buffer_size)
 while (conteudo):
+
+    # Header: bit de controle + delimitador
     header = bit + '$*$'
-    sent = sock.sendto(header.encode() + conteudo, dest)
-    tempo = time.time()
-    sock.settimeout(5)
+
+    # Selecao de numero randomico para gerar perda de pacote
+    if random.randint(0,20) <> 7: 
+        sent = sock.sendto(header.encode() + conteudo, dest)
+    
+    # Define um timeout para o socket
+    sock.settimeout(0.1)
     resp = ''
 
+    # Espera a resposta do outro host. Se nada eh recebido, volta para o inicio
+    # da iteracao e reenvia o pacote.
     try: 
         resp, address = sock.recvfrom(1024)
     except socket.timeout:
@@ -55,15 +68,22 @@ while (conteudo):
 
     print(resp)
     if resp <> '':
+        # Se o bit do ACK eh o mesmo que foi enviado, retorna para o inicio da 
+        # iteracao e reenvia o pacote. O bit esperado eh o bit seguinte.
+        # Ex.: se enviou pacote com bit 0, aguarda ACK com bit 1.
         if resp[4] == bit:
+            print('a')
             continue
         if bit == '1':
             bit = '0'
         else:
             bit = '1'
+
+    # Le mais um pedaco da imagem
     conteudo = arquivo_imagem.read(buffer_size)
     #time.sleep(1)
 
+# Envia a mensagem de finalizacao da transferencia
 sent = sock.sendto(fim.encode(), dest)
 sock.close()
 
